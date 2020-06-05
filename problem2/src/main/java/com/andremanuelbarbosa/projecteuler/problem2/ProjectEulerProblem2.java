71e7a109b45490400219a0a94af370d9b4180244
Andre Barbosa <andre.barbosa@jivesoftware.com>
2020-06-05 17:03:35 +0100

    Added the Solution to Problem 2.

---
+++ problem2/src/main/java/com/andremanuelbarbosa/projecteuler/problem2/ProjectEulerProblem2.java
@@ -0,0 +1,22 @@
+package com.andremanuelbarbosa.projecteuler.problem2;
+
+public class ProjectEulerProblem2 {
+
+    public int solution() {
+
+        int sum = 0;
+        int fibonacciCurrent = 2;
+        int fibonacciPrevious = 1;
+
+        while(fibonacciCurrent <= 4000000) {
+            int fibonacciNext = fibonacciCurrent + fibonacciPrevious;
+            fibonacciPrevious = fibonacciCurrent;
+            if(fibonacciCurrent % 2 == 0) {
+                sum += fibonacciCurrent;
+            }
+            fibonacciCurrent = fibonacciNext;
+        }
+
+        return sum;
+    }
+}
---
+++ problem2/src/test/java/com/andremanuelbarbosa/projecteuler/problem2/ProjectEulerProblem2Test.java
@@ -0,0 +1,24 @@
+package com.andremanuelbarbosa.projecteuler.problem2;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ProjectEulerProblem2Test {
+
+    private ProjectEulerProblem2 projectEulerProblem2;
+
+    @Before
+    public void setUp() {
+
+        projectEulerProblem2 = new ProjectEulerProblem2();
+    }
+
+    @Test
+    public void shouldReturnSolution() {
+
+        System.out.println(projectEulerProblem2.solution());
+    }
+}
